b506545dfcd80c28c3238d2dfee3bd47c93425c8
MisyuchenkoAS <misucenkoana@gmail.com>
2025-12-09 21:57:11 +0300

    docs: настроен conf.py для автодокументации
    Добавлены настройки:
    1. Путь к исходному коду проекта
    2. Расширения autodoc и napoleon для работы с docstrings
    3. Тема sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -5,24 +5,31 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os     # модуль для работы с файловой системой
+import sys    # модуль для работы с системными путями
+sys.path.insert(0, os.path.abspath('../'))  # добавляет путь на один уровень выше
 
 project = 'Шашки на PyGame'
-copyright = '2025, Команда разработчиков'
-author = 'Команда разработчиков'
+copyright = '2025, Команда разработчиков: Матвеев, Мисюченко, Жданова, Овчинникова, Бубнов'
+author = 'Команда разработчиков: Матвеев, Мисюченко, Жданова, Овчинникова, Бубнов'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# список расширений Sphinx
+extensions = [
+    'sphinx.ext.autodoc',   # автоматически извлекает документацию из docstrings
+    'sphinx.ext.napoleon',  # понимает Google Style и NumPy Style docstrings
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ['_templates']  # путь к пользовательским шаблонам HTML
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']  # файлы и папки, которые Sphinx будет игнорировать
 
 language = 'ru'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'  # тема оформления
 html_static_path = ['_static']
